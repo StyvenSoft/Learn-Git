0d59cf7ee4195d06640c597ccd5ae965c3730b2d
Steveen <steveen.echeverri@gmail.com>
2020-03-26 12:59:38 -0500

    Commands basic GIT

---
+++ BasicGit.sh
@@ -0,0 +1,26 @@
+#La palabra init significa inicializar .
+
+$ git init
+
+# verificar el estado de esos cambios 
+
+$ git status
+
+git status
+
+# Podemos agregar un archivo al área de preparación 
+
+$ git add filename
+
+# podemos verificar las diferencias entre el directorio de trabajo y el área de preparación 
+
+$ git diff filename
+
+# Un commit es el último paso en nuestro flujo de trabajo de Git.
+
+# la opción -m seguida de un mensaje
+
+$ git commit -m "Complete first line of dialogue"
+
+# Los commits se almacenan cronológicamente en el repositorio
+
---
+++ Readme.md
@@ -0,0 +1,25 @@
+**Basic Git Workflow**
+
+LESSON 1
+
+An introduction to Git and a few of its core features.
+
+Exercises
+
+- [ ] 1. Hello Git
+
+- [ ] 2. git init
+
+- [ ] 3. Git Workflow
+
+- [ ] 4. git status
+
+- [ ] 5. git add
+
+- [ ] 6. git diff
+
+- [ ] 7. git commit
+
+- [ ] 8. git log
+
+- [ ] 9. Generalizations
